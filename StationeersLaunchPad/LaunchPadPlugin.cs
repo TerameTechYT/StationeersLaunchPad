da71ef1f482faf3c2aaace1674a995ca10544f79
vivien <terametech@outlook.com>
2025-07-20 12:54:38 -0400

    add TryGetArg patch

--- StationeersLaunchPad/LaunchPadPlugin.cs
+++ StationeersLaunchPad/LaunchPadPlugin.cs
@@ -7,6 +7,7 @@ using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using Cysharp.Threading.Tasks;
 using HarmonyLib;
+using Networking;
 using Steamworks;
 using System;
 using System.IO;
@@ -17,6 +18,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.LowLevel;
+using Util.Commands;
 using GameString = Assets.Scripts.Localization2.GameString;
 
 namespace StationeersLaunchPad
@@ -366,6 +368,33 @@ namespace StationeersLaunchPad
             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "My Games") :
             StationSaveUtils.ExeDirectory.FullName,
           LaunchPadConfig.SavePath);
+      return false;
+    }
+
+    [HarmonyPatch(typeof(CommandLine), nameof(CommandLine.TryGetArg)), HarmonyPrefix]
+    static bool CommandLine_TryGetArg(ref bool __result, string arg, out string value)
+    {
+      if (CommandLine.CommandLineArgs == null)
+      {
+        value = null;
+        __result = false;
+        return false;
+      }
+      for (var i = 0; i < CommandLine.CommandLineArgs.Length; i++)
+      {
+        if (CommandLine.CommandLineArgs[i] == arg)
+        {
+          // make sure we check to make sure we are in array bounds
+          if (CommandLine.CommandLineArgs.Length < i + 1)
+            break;
+
+          value = CommandLine.CommandLineArgs[i + 1].Trim();
+          __result = true;
+          return false;
+        }
+      }
+      value = null;
+      __result = false;
       return false;
     }
   }
