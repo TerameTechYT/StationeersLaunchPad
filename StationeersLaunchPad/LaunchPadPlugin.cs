4a2824fcd33fab7f4b20939efbdf5e7e2fd1d8c5
Tom Holmes <tholmes150@gmail.com>
2025-09-14 17:05:58 -0500

    bump version to 0.2.11

--- StationeersLaunchPad/LaunchPadPlugin.cs
+++ StationeersLaunchPad/LaunchPadPlugin.cs
@@ -27,7 +27,7 @@ namespace StationeersLaunchPad
   {
     public const string pluginGuid = "stationeers.launchpad";
     public const string pluginName = "StationeersLaunchPad";
-    public const string pluginVersion = "0.2.10";
+    public const string pluginVersion = "0.2.11";
 
     void Awake()
     {
