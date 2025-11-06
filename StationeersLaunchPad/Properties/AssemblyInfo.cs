49885ac8d5467f200a7f7794a2aea30af5185f21
vivi <terametech@outlook.com>
2025-11-06 10:07:08 -0500

    Update build system

---
+++ StationeersLaunchPad/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using StationeersLaunchPad;
+using System.Reflection;
+
+[assembly: AssemblyTitle(LaunchPadPlugin.pluginName)]
+[assembly: AssemblyDescription(LaunchPadPlugin.pluginName)]
+[assembly: AssemblyCompany(LaunchPadPlugin.pluginName)]
+[assembly: AssemblyProduct(LaunchPadPlugin.pluginName)]
+[assembly: AssemblyVersion(LaunchPadPlugin.pluginVersion)]
+[assembly: AssemblyFileVersion(LaunchPadPlugin.pluginVersion)]
